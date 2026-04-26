ae1030856010fdcc1a6acbd4321cafa5e93ca2a2
DafferNx <filbertdeven20@gmail.com>
2026-04-26 15:19:54 +0700

    Menambahkan Implementasi Runtime Configuration

---
+++ TP_MODUL9_103022400112/CovidConfig.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Text.Json;
+
+public class CovidConfig
+{
+	public string satuan_suhu {  get; set; }
+	public int batas_hari_demam { get; set; }
+	public string pesan_ditolak {  get; set; }
+    public string pesan_diterima { get; set; }
+
+	private string jsonPath = "D:/KPL/TP_MODUL9_103022400112/" +
+        "TP_MODUL9_103022400112/obj/Debug/net10.0/covid_config.json";
+    private class ConfigPath
+    {
+        public string satuan_suhu { get; set; }
+        public string batas_hari_deman { get; set; }
+        public string pesan_ditolak { get; set; }
+        public string pesan_diterima { get; set; }
+    }
+
+    public CovidConfig()
+    {
+        if (!File.Exists(jsonPath))
+        {
+            DefaultConfig();
+        }
+
+        LoadConfig();
+    }
+
+    private void DefaultConfig()
+    {
+        ConfigPath confPath = new ConfigPath
+        {
+            satuan_suhu = "config1.json",
+            batas_hari_deman = "config2.json",
+            pesan_ditolak = "config3.json",
+            pesan_diterima = "config4.json"
+        };
+
+        File.WriteAllText(jsonPath, JsonSerializer.Serialize(confPath, new JsonSerializerOptions { WriteIndented = true }));
+
+        File.WriteAllText("config1.json", JsonSerializer.Serialize("celcius"));
+        File.WriteAllText("config2.json", JsonSerializer.Serialize(14));
+        File.WriteAllText("config3.json", JsonSerializer.Serialize("Anda tidak diperbolehkan masuk ke dalam gedung ini"));
+        File.WriteAllText("config4.json", JsonSerializer.Serialize("Anda dipersilahkan untuk masuk ke dalam gedung ini"));
+    }
+
+    public void LoadConfig()
+    {
+        ConfigPath path = JsonSerializer.Deserialize<ConfigPath>(File.ReadAllText(jsonPath));
+
+        satuan_suhu = JsonSerializer.Deserialize<string>(File.ReadAllText(path.satuan_suhu));
+        batas_hari_demam = JsonSerializer.Deserialize<int>(File.ReadAllText(path.batas_hari_deman));
+        pesan_ditolak = JsonSerializer.Deserialize<string>(File.ReadAllText(path.pesan_ditolak));
+        pesan_diterima = JsonSerializer.Deserialize<string>(File.ReadAllText(path.pesan_diterima));
+    }
+
+    public void UbahSatuan()
+    {
+        string satuanBaru;
+
+        if (satuan_suhu == "celcius")
+            satuanBaru = "fahrenheit";
+        else
+            satuanBaru = "celcius";
+
+        File.WriteAllText("config1.json", JsonSerializer.Serialize(satuanBaru));
+
+        LoadConfig();
+    }
+}
--- TP_MODUL9_103022400112/Program.cs
+++ TP_MODUL9_103022400112/Program.cs
@@ -1,1 +1,37 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CovidConfig config = new CovidConfig();
+
+        config.UbahSatuan();
+
+        Console.WriteLine($"Berapa suhu badan anda saat ini? Dalam nilai {config.satuan_suhu}");
+        double suhu = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala deman? ");
+        int hariDemam = Convert.ToInt32(Console.ReadLine());
+
+        bool suhuSesuai = true;
+
+        if (config.satuan_suhu == "celcius")
+        {
+            suhuSesuai = (suhu >= 36.5 && suhu <= 37.5);
+        }
+        else if (config.satuan_suhu == "fahrenheit")
+        {
+            suhuSesuai = (suhu >= 97.7 && suhu <= 99.5);
+        }
+
+        if (suhuSesuai && hariDemam < config.batas_hari_demam)
+        {
+            Console.WriteLine(config.pesan_diterima);
+        }
+        else
+        {
+            Console.WriteLine(config.pesan_ditolak);
+        }
+    }
+}
